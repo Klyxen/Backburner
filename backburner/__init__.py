81d18e39e352987b8c657e853631781ea1486348
K7yn <81945767+Klyxen@users.noreply.github.com>
2025-05-03 21:21:13 +0800

    change 1

--- backburner/__init__.py
+++ backburner/__init__.py
@@ -1,1 +1,5 @@
+__version__ = "2.0.0"
 
+from .config import BackburnerConfig
+from .scanner import scan_target_ports, resolve_ip, grab_banner
+from .utils import print_message, parse_target, is_valid_target, save_results
