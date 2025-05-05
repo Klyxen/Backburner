39255ababefedc123f0e029f77c7f9cc08019034
K7yn <81945767+Klyxen@users.noreply.github.com>
2025-05-05 17:46:48 +0800

    Update cli.py

--- backburner/cli.py
+++ backburner/cli.py
@@ -13,10 +13,13 @@ def clear_terminal() -> None:
 def display_banner() -> None:
     """Display the Backburner banner."""
     banner = """
+    _--____-__---____---______----____-___-_____-____-----___`.
     ╔════════════════════════════╗
           BACKBURNER
     ╚════════════════════════════╝
     created bt : Ky1e/Klyxen
+
+    --__----_--___----___------____---_---_-----_----_____---`.
     """
     print_message(Fore.LIGHTRED_EX + Style.BRIGHT + banner + Style.RESET_ALL)
 
